02422738e984b2f51130481687508ed5c3f5e8af
valeronchikchik <tsanko.valerii@student.uzhnu.edu.ua>
2026-03-06 08:26:51 +0200

    if_else_add

--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -13,7 +13,28 @@ namespace lab_2
                 Console.WriteLine("Введіть c: ");
                 double c = Convert.ToDouble(Console.ReadLine());
 
-            double result = (Math.Min(a, b) + Math.Pow(Math.Max(b, c), 2));
+            double firstPart = 0;
+            double secondPart = 0;
+
+            if (a < b)
+            {
+                firstPart = a;
+            }
+            else
+            {
+                firstPart = b;
+            }
+
+            if (b > c)
+            {
+                secondPart = Math.Pow(b, 2);
+            }
+            else
+            {
+                secondPart = Math.Pow(c, 2);
+            }
+
+            double result = firstPart + secondPart;
 
             Console.WriteLine($"Результат: {result}");
 
