24caf0ae114c92e3f779ff87e142c6e39fc33858
valeronchikchik <tsanko.valerii@student.uzhnu.edu.ua>
2026-03-06 08:44:53 +0200

    add_task2_3

---
+++ Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void main()
+        {
+            Console.WriteLine("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int y = 0;
+
+            if (0<=n || n < 5) 
+            {
+                y = 0;
+            }
+            else if (5 <= n || n < 10)
+            {
+                y = 1;
+            }
+            else if (10 <= n || n < 15)
+            {
+                y = 2;
+            }
+            else
+            {
+                y = 3;
+            }
+
+
+             Console.WriteLine($"y: {y}");
+             return 0;
+        }
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task3 
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1: ")
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter z1: ");
+            int z1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter z2: ");
+            int z2 = Convert.ToInt32(Console.ReadLine());
+
+            string result;
+
+            if((x2 - x1) > (y2 - y1) || (x2-x1) > (z2-z1))
+            {
+                result = "Сторона x найбільша";
+            }
+            else if((y2 - y1) > (x2 - x1) || (y2-y1) > (z2-z1))
+            {
+                result = "Сторона y найбільша";
+            }
+            else
+            {
+                result = "Сторона z найбільша";
+            }
+        }
+    }   
+}
+
