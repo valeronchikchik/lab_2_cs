9cbfb9fbc9e2ade6669f8ba40a0b30c06904438c
valeronchikchik <tsanko.valerii@student.uzhnu.edu.ua>
2026-03-06 08:17:17 +0200

    Add project files.

---
+++ lab_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Введіть a: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введіть b: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введіть c: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+            double result = (Math.Min(a, b) + Math.Pow(Math.Max(b, c), 2));
+
+            Console.WriteLine($"Результат: {result}");
+
+        }
+    }
+}
